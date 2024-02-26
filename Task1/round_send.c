17109ee2404b037aeaa70930fc8f0253bb4ce704
sovus_tartar <vad-nesmejanov@yandex.ru>
2024-02-26 21:36:03 +0000

    Task1 done

---
+++ Task1/round_send.c
@@ -0,0 +1,52 @@
+#include <mpi/mpi.h>
+#include <mpich/mpi_proto.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Num_of_cores must be less then 10
+
+void executer(int rank, int num_of_executers)
+{
+
+    if(rank == 0)
+    {
+        char str[] = "Message went through 0";
+
+        MPI_Send(str, strlen(str) + 1, MPI_CHAR, (rank + 1) % num_of_executers, 1, MPI_COMM_WORLD);
+        printf("Executer %d: sent str \"%s\"\n", rank, str);
+        char buffer[100];
+        MPI_Status status;
+
+        MPI_Recv(buffer, 100, MPI_CHAR, num_of_executers - 1, 1, MPI_COMM_WORLD, &status);
+        printf("Executer %d: received str \"%s\"\n", rank, buffer);
+        puts(buffer);
+
+    }
+    else if (rank < num_of_executers)
+    {
+        char buffer[100];
+        MPI_Status status;
+        MPI_Recv(buffer, 100, MPI_CHAR, rank - 1, 1, MPI_COMM_WORLD, &status);
+        printf("Executer %d: received str \"%s\"\n", rank, buffer);
+        char str[2] = {rank + '0', 0};
+        strcat(buffer, str);
+        
+        MPI_Send(buffer, strlen(buffer) + 1, MPI_CHAR, (rank + 1) % num_of_executers, 1, MPI_COMM_WORLD);
+        printf("Executer %d: sent str \"%s\"\n", rank, buffer);
+    }
+
+}
+
+int main(int argc, char ** argv)
+{
+    int commsize, my_rank;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &commsize); 
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    
+    executer(my_rank, commsize);
+
+    MPI_Finalize();
+}
---
+++ Task1/row_sum.c
@@ -0,0 +1,49 @@
+#include <mpi/mpi.h>
+#include <mpich/mpi_proto.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void executer(int rank, int num_of_executers, int N)
+{
+    double sum = 0;
+
+    // printf("Exectuter %d: N=%d\n", rank, N);
+
+    for(int i = rank + 1; i <= N; i+=num_of_executers)
+        sum += 1. / i;
+
+    if(rank != 0)
+    {
+        MPI_Send(&sum, 1, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+        // printf("Executer %d: sent sum=%lf to main\n", rank, sum);
+    }
+    else
+    {
+        for(int i = 1; i < num_of_executers; ++i)
+        {
+            double temp_sum = 0;
+            MPI_Status status;
+            MPI_Recv(&temp_sum, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            // printf("Executer %d: recieved sum = %lf\n", rank, temp_sum);
+            sum += temp_sum;
+        }    
+
+        printf("Executer %d: sum = %lf\n", rank, sum);
+    }
+
+}
+
+int main(int argc, char ** argv)
+{
+    int commsize, my_rank;
+
+    int N = atoi(argv[1]);
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &commsize); 
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    
+    executer(my_rank, commsize, N);
+
+    MPI_Finalize();
+}
